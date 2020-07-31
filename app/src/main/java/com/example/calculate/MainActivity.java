d967ac4ccfdf576c0f61f8853e16492e8415631b
linaiwang <linw0701@thundersoft.com>
2020-07-31 11:07:25 +0800

    calculate2.0

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -10,7 +11,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView result;
-    private String data="";
+    double var1,var2;
+    boolean add,sub,mul,div;
+    String s;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView text_8=(TextView)findViewById(R.id.text_8);
         TextView text_9=(TextView)findViewById(R.id.text_9);
         TextView text_0=(TextView)findViewById(R.id.text_0);
+        TextView text_c=(TextView)findViewById(R.id.text_clear);
+        TextView text_add=(TextView)findViewById(R.id.text_add);
+        TextView text_minus=(TextView)findViewById(R.id.text_minus);
+        TextView text_multiply=(TextView)findViewById(R.id.text_multiply);
+        TextView text_divide=(TextView)findViewById(R.id.text_divide);
+        TextView text_dot=(TextView)findViewById(R.id.text_dot);
+        TextView text_equal=(TextView)findViewById(R.id.equal);
+        TextView text_back=(TextView)findViewById(R.id.text_backspace);
 
         text_1.setOnClickListener(this);
         text_2.setOnClickListener(this);
@@ -41,13 +53,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         text_8.setOnClickListener(this);
         text_9.setOnClickListener(this);
         text_0.setOnClickListener(this);
+        text_c.setOnClickListener(this);
+        text_dot.setOnClickListener(this);
+        text_add.setOnClickListener(this);
+        text_divide.setOnClickListener(this);
+        text_equal.setOnClickListener(this);
+        text_minus.setOnClickListener(this);
+        text_multiply.setOnClickListener(this);
+        text_back.setOnClickListener(this);
         result = (TextView)findViewById(R.id.result_text);
-//        text_1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                result.setText("1");
-//            }
-//        });
 
     }
 
@@ -55,54 +69,90 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.text_0:
-                String a0="0";
-                data=data+a0;
-                result.setText(data);
+                result.setText(result.getText()+"0");
                 break;
             case R.id.text_1:
-                String a1="1";
-                data=data+a1;
-                result.setText(data);
+                result.setText(result.getText()+"1");
                 break;
             case R.id.text_2:
-                String a2="2";
-                data=data+a2;
-                result.setText(data);
+                result.setText(result.getText()+"2");
                 break;
             case R.id.text_3:
-                String a3="3";
-                data=data+a3;
-                result.setText(data);
+                result.setText(result.getText()+"3");
                 break;
             case R.id.text_4:
-                String a4="4";
-                data=data+a4;
-                result.setText(data);
+                result.setText(result.getText()+"4");
                 break;
             case R.id.text_5:
-                String a5="5";
-                data=data+a5;
-                result.setText(data);
+                result.setText(result.getText()+"5");
                 break;
             case R.id.text_6:
-                String a6="6";
-                data=data+a6;
-                result.setText(data);
+                result.setText(result.getText()+"6");
                 break;
             case R.id.text_7:
-                String a7="7";
-                data=data+a7;
-                result.setText(data);
+                result.setText(result.getText()+"7");
                 break;
             case R.id.text_8:
-                String a8="8";
-                data=data+a8;
-                result.setText(data);
+                result.setText(result.getText()+"8");
                 break;
             case R.id.text_9:
-                String a9="9";
-                data=data+a9;
-                result.setText(data);
+                result.setText(result.getText()+"9");
+                break;
+            case R.id.text_clear:
+                result.setText("");
+                var1=0;
+                var2=0;
+                add=false;
+                sub=false;
+                mul=false;
+                div=false;
+                break;
+            case R.id.text_dot:
+                result.setText(result.getText()+".");
+                break;
+            case R.id.text_divide:
+                var1=Double.parseDouble(result.getText()+"");
+                div=true;
+                result.setText(null);
+                break;
+            case R.id.text_add:
+                var1=Double.parseDouble(result.getText()+"");
+                add=true;
+                result.setText(null);
+                break;
+            case R.id.text_minus:
+                var1=Double.parseDouble(result.getText()+"");
+                sub=true;
+                result.setText(null);
+                break;
+            case R.id.text_multiply:
+                var1=Double.parseDouble(result.getText()+"");
+                mul=true;
+                result.setText(null);
+                break;
+            case R.id.equal:
+                var2=Double.parseDouble(result.getText()+"");
+                if (add==true){
+                    result.setText(var1+var2+"");
+                    add=false;
+                }
+                if (sub==true){
+                    result.setText(var1-var2+"");
+                    sub=false;
+                }
+                if (mul==true){
+                    result.setText(var1*var2+"");
+                    mul=false;
+                }
+                if (div==true){
+                    result.setText(var1/var2+"");
+                    div=false;
+                }
+                break;
+            case R.id.text_backspace:
+                s=result.getText()+"";
+                s=s.substring(0,s.length()-1);
+                result.setText(s);
                 break;
             default:
                 break;
